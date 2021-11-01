fd75c1d3a0c445e58b63dccb82bceb3d1449d886
Yacon <phungtanphat23@gmail.com>
2021-11-01 15:28:54 +0700

    Switch to Nextcord, and switch from Dpymenus to Nextcord.ext.menus

--- cogs/Logistics.py
+++ cogs/Logistics.py
@@ -1,14 +1,48 @@
 from os import getenv
 from typing import Optional
 
+from nextcord import Embed
 from aiohttp import ClientSession
-from discord.ext.commands import (Cog, command, has_role)
-from discord.ext.commands.errors import MissingRole
+from nextcord.ext.commands import Cog, command, has_role
+from nextcord.ext.commands.errors import MissingRole
 from pymongo import MongoClient
 from roblox import Client
-from dpymenus import Page, PaginatedMenu
+from nextcord.ext.menus import ListPageSource, MenuPages
+from requests import get
 
 from . import del_user_msg
+
+
+class HelpMenu(ListPageSource):
+    def __init__(self, ctx, data, userName):
+        self.ctx = ctx
+        self.userName = userName
+
+        super().__init__(data, per_page=6)
+
+
+    async def write_page(self, menu, fields=[]):
+        offset = (menu.current_page*self.per_page) + 1
+        len_data = len(self.entries)
+
+        # url = get(
+        #         f"https://thumbnails.roblox.com/v1/users/avatar?format=Png&isCircular=false&size=420x420&userIds={self.userName.id}").json()
+
+        embed = Embed(title=f"{self.userName}'s friends list", colour=0x2f3136)
+        embed.set_footer(text=f"{offset:,} - {min(len_data, offset+self.per_page-1):,} of {len_data:,} commands.")
+        # embed.set_thumbnail(url=url["data"][0]["imageUrl"]) 
+
+
+        for name, value in fields:
+            embed.add_field(name=name, value=value, inline=False)
+
+        return embed
+
+
+    async def format_page(self, menu, entries):
+        fields = [(f"Username: {name}", f"Display name: {displayName} \nID: {userID}", False) for name, displayName, userID in entries]
+
+        return await self.write_page(menu, fields)
 
 
 class Logistics(Cog):
@@ -20,8 +54,6 @@ class Logistics(Cog):
         self.DB = self.MONGO_CLIENT["RF911"]
         self.GUILD_DB = self.DB['Guild']
 
-        self.PAGE_LIST = []
-
 
     @staticmethod
     async def get_url(userID):
@@ -31,20 +63,10 @@ class Logistics(Cog):
             async with session.get(URL) as response:
                 url = await response.json()
                 data = url["data"]
+                friend_ids = [[data[i]["name"], data[i]["displayName"], data[i]["id"]] for i in range(len(data))]
 
-                friend_ids = [[data[i]["name"], data[i]["displayName"], data[i]["id"], str(data[i]["isOnline"])] for i in range(len(data))]
 
         return friend_ids
-
-
-    def auto_page(self, friend, target, page_index):
-        page = Page(title=f"{target.name}'s friends list", colour= 0x2f3136)
-        page.set_footer(text=page_index)
-
-        for name, displayname, friendID, is_online in friend:
-            page.add_field(name=f"Name: {name}", value=f"Display name: {displayname}\nIDs: {friendID}\nOnline: {is_online}\n[Link](https://www.roblox.com/users/{friendID}/profile)\n-----------------------------", inline=False)
-
-        self.PAGE_LIST.append(page)
 
 
     @command(name='check-friend', aliases=['cf'], description='Check user\'s friend list. Require ')
@@ -52,25 +74,17 @@ class Logistics(Cog):
     async def _check_friend(self, ctx, userName: Optional[str] = "roblox"):
         await del_user_msg(ctx)
 
-        user_name = await self.roblox.get_user_by_username(userName)
-        if user_name == None:
+        username = await self.roblox.get_user_by_username(userName)
+        if username == None:
             await ctx.send("No user found with that username.")
         else:
-            friend_ids = await self.get_url(user_name.id)
+            friend_ids = await self.get_url(username.id)
             if len(friend_ids):
-                self.PAGE_LIST = []
-                index, page, per_page = 0, 1, 6
-                while index < len(friend_ids):
-                    page_index = f" {page} - {(len(friend_ids)//per_page + 1) if len(friend_ids)%per_page else len(friend_ids)//per_page} in {len(friend_ids)} friends."
-                    self.auto_page(friend_ids[index:index + per_page ], user_name, page_index)
-                    index += 6; page += 1
+                menu = MenuPages(source=HelpMenu(ctx, friend_ids, username),
+                             clear_reactions_after=True,
+                             timeout=60.0)
+                await menu.start(ctx)
 
-                menu = (PaginatedMenu(ctx)
-                            .set_timeout(60)
-                            .add_pages(self.PAGE_LIST)
-                            .show_skip_buttons()
-                       )
-                await menu.open()
             else:
                 await ctx.send("This user has no friends")
 
