3083ff178d5dc2734dafe1f6f5154231e70716f9
Yacon <phungtanphat23@gmail.com>
2021-11-07 17:13:10 +0700

    Add description, using new CustomButtonMenuPages

--- cogs/Logistics.py
+++ cogs/Logistics.py
@@ -1,5 +1,6 @@
 from os import getenv
 from typing import Optional
+from requests import get
 
 from nextcord import Embed
 from aiohttp import ClientSession
@@ -7,10 +8,9 @@ from nextcord.ext.commands import Cog, command, has_role
 from nextcord.ext.commands.errors import MissingRole
 from pymongo import MongoClient
 from roblox import Client
-from nextcord.ext.menus import ListPageSource, ButtonMenuPages
-from requests import get
+from nextcord.ext.menus import ListPageSource
 
-from . import del_user_msg
+from . import del_user_msg, CustomButtonMenuPages
 
 
 class HelpMenu(ListPageSource):
@@ -18,29 +18,33 @@ class HelpMenu(ListPageSource):
         self.ctx = ctx
         self.userName = userName
 
-        super().__init__(data, per_page=6)
+        super().__init__(data, per_page=18)
 
 
     async def write_page(self, menu, fields=[]):
         offset = (menu.current_page*self.per_page) + 1
         len_data = len(self.entries)
 
-        # url = get(
-        #         f"https://thumbnails.roblox.com/v1/users/avatar?format=Png&isCircular=false&size=420x420&userIds={self.userName.id}").json()
+        url = get(f"https://thumbnails.roblox.com/v1/users/avatar?format=Png&isCircular=false&size=420x420&userIds={self.userName.id}").json()
 
-        embed = Embed(title=f"{self.userName.name}'s friends list", colour=0x2f3136)
-        embed.set_footer(text=f"{offset:,} - {min(len_data, offset+self.per_page-1):,} of {len_data:,} commands.")
-        # embed.set_thumbnail(url=url["data"][0]["imageUrl"]) 
+        embed = Embed(title=f"{self.userName.name.capitalize()}'s friends list", colour=0x2f3136)
+        embed.set_footer(text=f"{offset:,} - {min(len_data, offset+self.per_page-1):,} of {len_data:,} friends.")
+        embed.set_thumbnail(url=url["data"][0]["imageUrl"])
 
+        field = [("User Name: ", self.userName.name, True),
+                ("Display Name: ", self.userName.display_name, True),
+                ("ID: ", self.userName.id, False),]
+        
+        for name, value, inline in field:
+            embed.add_field(name=name, value=value, inline=inline)
 
         for name, value, inline in fields:
-            embed.add_field(name=name, value=value, inline=False)
+            embed.add_field(name=name, value=value, inline=True)
 
         return embed
 
-
     async def format_page(self, menu, entries):
-        fields = [(f"Username: {name}", f"Display name: {displayName} \nID: {userID}", False) for name, displayName, userID in entries]
+        fields = [(f"Username: \n{name}", f"Display name: \n{displayName} \nID: {userID}", False) for name, displayName, userID in entries]
 
         return await self.write_page(menu, fields)
 
@@ -69,7 +73,7 @@ class Logistics(Cog):
         return friend_ids
 
 
-    @command(name='check-friend', aliases=['cf'], description='Check user\'s friend list. Require ')
+    @command(name='check-friend', aliases=['cf'], description='Check user\'s friend list. Require `Logistics` Role')
     @has_role("Logistics")
     async def _check_friend(self, ctx, userName: Optional[str] = "roblox"):
         await del_user_msg(ctx)
@@ -80,9 +84,8 @@ class Logistics(Cog):
         else:
             friend_ids = await self.get_url(username.id)
             if len(friend_ids):
-                menu = ButtonMenuPages(source=HelpMenu(ctx, friend_ids, username),
-                             clear_reactions_after=True,
-                             timeout=60.0)
+                menu = CustomButtonMenuPages(source=HelpMenu(ctx, friend_ids, username),
+                             timeout=120.0)
                 await menu.start(ctx)
 
             else:
