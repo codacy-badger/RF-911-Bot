1f52a05562f93e0b7a2d692b939c5fb69656012c
Yacon <phungtanphat23@gmail.com>
2021-11-07 16:58:58 +0700

    Add CustomButtonMenuPages

--- cogs/__init__.py
+++ cogs/__init__.py
@@ -1,4 +1,44 @@
+from nextcord import ui, ButtonStyle
 from nextcord.ext.commands import Cog
+from nextcord.ext.menus import ButtonMenuPages, MenuPaginationButton
+
+from . import *
+
+class CustomButtonMenuPages(ButtonMenuPages, inherit_buttons=False):
+    """
+    This class overrides the default ButtonMenuPages without inheriting the buttons
+    by setting inherit_buttons to False.
+    This allows us to add our own buttons with custom labels, styles, or even callbacks. 
+    """
+
+    def __init__(self, source, timeout):
+        super().__init__(source, timeout=timeout, delete_message_after=True, clear_reactions_after=True)
+
+        self.FIRST_PAGE = "<:end_back:906578658146254858>"
+        self.LAST_PAGE = "<:end_next:906578658108522586>"
+        self.PREVIOUS_PAGE = "<:previous:906578658062385192>"
+        self.NEXT_PAGE = "<:next:906578657827512362>"
+        self.STOP = "<:stop:906578657890418820>"
+
+        # You can change the buttons to have custom labels and styles by setting
+        # inherit_buttons=False above and adding them with self.add_item as shown below.
+        # Note: None of the buttons are required you can leave any of them out
+        self.add_item(MenuPaginationButton(emoji=self.FIRST_PAGE, label="First", style=ButtonStyle.success))
+        self.add_item(MenuPaginationButton(emoji=self.NEXT_PAGE, label="Next", style=ButtonStyle.blurple))
+        self.add_item(MenuPaginationButton(emoji=self.LAST_PAGE, label="Last", style=ButtonStyle.success))
+        self.add_item(MenuPaginationButton(emoji=self.PREVIOUS_PAGE, label="Prev", style=ButtonStyle.blurple))
+        
+        # Rearrange the buttons (place the Stop button (first button) at the end of the list)
+        self.children = [self.children[1], self.children[4], self.children[0], self.children[2], self.children[3]]
+        
+        # Disable buttons that are unavailable to be pressed at the start
+        self._disable_unavailable_buttons()
+
+    # To change the callback function, we can use the button decorator
+    @ui.button(emoji='<:stop:906578657890418820>', label="Stop", style=ButtonStyle.red)
+    async def stop_button(self, button, interaction):
+        # await interaction.response.send_message("You pressed stop.", ephemeral=True)
+        self.stop()
 
 
 async def del_user_msg(ctx):
