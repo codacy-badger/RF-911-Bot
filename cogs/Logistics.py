bc912814c7053f8a7700c5692a36a1237e17e744
Yacon <phungtanphat23@gmail.com>
2021-11-08 11:48:15 +0700

    Change page display, add direct host for logistics, sort import

--- cogs/Logistics.py
+++ cogs/Logistics.py
@@ -1,19 +1,20 @@
+from datetime import datetime
 from os import getenv
 from typing import Optional
-from requests import get
 
-from nextcord import Embed
 from aiohttp import ClientSession
+from nextcord import Embed
 from nextcord.ext.commands import Cog, command, has_role
 from nextcord.ext.commands.errors import MissingRole
-from pymongo import MongoClient
-from roblox import Client
 from nextcord.ext.menus import ListPageSource
+from pymongo import MongoClient
+from requests import get
+from roblox import Client
 
-from . import del_user_msg, CustomButtonMenuPages
+from . import CustomButtonMenuPages, del_user_msg
 
 
-class HelpMenu(ListPageSource):
+class FriendMenu(ListPageSource):
     def __init__(self, ctx, data, userName):
         self.ctx = ctx
         self.userName = userName
@@ -22,21 +23,17 @@ class HelpMenu(ListPageSource):
 
 
     async def write_page(self, menu, fields=[]):
-        offset = (menu.current_page*self.per_page) + 1
-        len_data = len(self.entries)
+        current_page = menu.current_page + 1
+        max_page = round(len(self.entries) / self.per_page) + 1
 
         url = get(f"https://thumbnails.roblox.com/v1/users/avatar?format=Png&isCircular=false&size=420x420&userIds={self.userName.id}").json()
 
-        embed = Embed(title=f"{self.userName.name.capitalize()}'s friends list", colour=0x2f3136)
-        embed.set_footer(text=f"{offset:,} - {min(len_data, offset+self.per_page-1):,} of {len_data:,} friends.")
+        embed = Embed(title=f"{self.userName.name.capitalize()}'s friends list", 
+                      colour=0x2f3136, 
+                      description=f"User Name: {self.userName.name}\nDisplay Name: {self.userName.display_name}\nID: {self.userName.id}"
+                      )
+        embed.set_footer(text=f"Page {current_page}/{max_page}.")
         embed.set_thumbnail(url=url["data"][0]["imageUrl"])
-
-        field = [("User Name: ", self.userName.name, True),
-                ("Display Name: ", self.userName.display_name, True),
-                ("ID: ", self.userName.id, False),]
-        
-        for name, value, inline in field:
-            embed.add_field(name=name, value=value, inline=inline)
 
         for name, value, inline in fields:
             embed.add_field(name=name, value=value, inline=True)
@@ -45,7 +42,7 @@ class HelpMenu(ListPageSource):
 
     async def format_page(self, menu, entries):
         fields = [(f"Username: \n{name}", f"Display name: \n{displayName} \nID: {userID}", False) for name, displayName, userID in entries]
-
+        
         return await self.write_page(menu, fields)
 
 
@@ -57,6 +54,16 @@ class Logistics(Cog):
         self.MONGO_CLIENT =  MongoClient(getenv("DATABASE"))
         self.DB = self.MONGO_CLIENT["RF911"]
         self.GUILD_DB = self.DB['Guild']
+
+    
+    async def get_hitlist(self, ctx):
+        HITLIST = self.GUILD_DB.find_one({"_id": ctx.guild.id})
+        
+        if HITLIST is None:
+            return None
+        else:
+            CHANNEL = self.bot.get_channel(HITLIST["Hitlist"])
+            return CHANNEL
 
 
     @staticmethod
@@ -75,7 +82,7 @@ class Logistics(Cog):
 
     @command(name='check-friend', aliases=['cf'], description='Check user\'s friend list. Require `Logistics` Role')
     @has_role("Logistics")
-    async def _check_friend(self, ctx, userName: Optional[str] = "roblox"):
+    async def check_friend_command(self, ctx, userName: str):
         await del_user_msg(ctx)
 
         username = await self.roblox.get_user_by_username(userName)
@@ -84,15 +91,44 @@ class Logistics(Cog):
         else:
             friend_ids = await self.get_url(username.id)
             if len(friend_ids):
-                menu = CustomButtonMenuPages(source=HelpMenu(ctx, friend_ids, username),
+                menu = CustomButtonMenuPages(source=FriendMenu(ctx, friend_ids, username),
                              timeout=120.0)
                 await menu.start(ctx)
 
             else:
                 await ctx.send("This user has no friends")
 
+    
+    @command(name='host-bounty', aliases=['hb'], description='Host bounty without submission. Require `Logistics` Role')
+    @has_role("Logistics")
+    async def host_bounty_command(self, ctx, userName: str, reason: str):
+        await del_user_msg(ctx)
 
-    @_check_friend.error
+        user_name = await self.roblox.get_user_by_username(userName)
+        if user_name == None:
+            await ctx.send("No user found with that username.")
+        else:
+            user = await self.roblox.get_user(user_name.id)
+            url = get(
+                f"https://thumbnails.roblox.com/v1/users/avatar?format=Png&isCircular=false&size=420x420&userIds={user_name.id}").json()
+
+            embed = Embed(title=f"{user.name} profiles", color=0x2f3136, url=f"https://www.roblox.com/users/{user.id}/profile", timestamp=datetime.utcnow())
+            embed.set_author(name=f"Resquested by {ctx.author}", icon_url=f'{ctx.author.display_avatar}')
+            embed.set_image(url=url["data"][0]["imageUrl"])
+
+            fields = [("User Name: ", user.name, True),
+                        ("Display Name: ", user.display_name, True),
+                        ("Created at: ", str(user.created)[:10], True),
+                        ("Reason: ", reason, False),]
+
+            for name, value, inline in fields:
+                embed.add_field(name=name, value=value, inline=inline)
+
+            hitlist = await self.get_hitlist(ctx)
+            await hitlist.send(embed=embed)
+
+
+    @check_friend_command.error
     async def _load_error(self, ctx, exc):
         if isinstance(exc, MissingRole):
             await ctx.send(content=exc, delete_after = 20)
