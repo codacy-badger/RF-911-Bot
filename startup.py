87686408fdda7c38f63ec66fb75fd25820da7630
Yacon <phungtanphat23@gmail.com>
2021-11-23 17:26:32 +0700

    Version: 0.8.0: Added uvloop for faster async

--- startup.py
+++ startup.py
@@ -1,8 +1,10 @@
-from bot import *
+from uvloop import install
+from RF.bot import *
 
-VERSION = "0.7.0"
+VERSION = "0.8.0"
 
 def main():
+    install()
     bot.run(VERSION)
 
 if __name__ == '__main__':
