8b2f16752768c2d2c803f5bd710a567b82b738a3
Yacon <phungtanphat23@gmail.com>
2021-12-11 01:07:18 +0700

    Update Bounty Systems using webhook instead of bot.

--- RF/cogs/Bounty.py
+++ RF/cogs/Bounty.py
@@ -1,12 +1,11 @@
 from datetime import datetime, timedelta
 from typing import Union
 
-from nextcord import (ButtonStyle, Embed, Interaction, Member, Message,
-                      TextChannel, ui)
+from nextcord import (ButtonStyle, Embed, Guild, Interaction, Member, Message,
+                      TextChannel, User, Webhook, WebhookMessage, ui)
 from nextcord.ext.commands import (BucketType, Cog, Context, command, cooldown,
                                    has_permissions, has_role)
 from nextcord.ext.menus import ButtonMenu, Menu
-from roblox import Client
 
 from ..bot import RF
 from . import del_user_msg
@@ -47,10 +46,9 @@ class ButtonConfirm(ButtonMenu):
 class Bounty(Cog):
     def __init__(self, bot: RF) -> None:
         self.bot = bot
-        self.roblox = Client()
 
 
-    async def check_channel(self, ctx) -> bool:
+    async def checkChannel(self, ctx: Context) -> bool:
         BOUNTY_SUBMISSIONS = self.bot.GUILD_DB.find_one({"_id": ctx.guild.id})['Bounty submission']
 
         if ctx.channel.id != BOUNTY_SUBMISSIONS and BOUNTY_SUBMISSIONS is not None:
@@ -58,55 +56,68 @@ class Bounty(Cog):
         return True
 
     
-    async def get_hitlist(self, ctx) -> TextChannel:
+    async def sendHitlist(self, ctx: Context, embed: Embed, host: Union[Member, User]) -> WebhookMessage:
         HITLIST = self.bot.GUILD_DB.find_one({"_id": ctx.guild.id})
-        
-        if HITLIST is None:
-            return None
-        else:
-            CHANNEL = self.bot.get_channel(HITLIST["Hitlist"])
-            return CHANNEL
+        webhook = HITLIST["Hitlist"]
+        ID, Token = webhook["ID"], webhook["Token"]
+
+        channel = Webhook.from_url(url=f"https://discord.com/api/webhooks/{ID}/{Token}", session=self.bot.session)
+        message = await channel.send(username=f"{host.name}", avatar_url=host.display_avatar, embed=embed, wait=True)
+        return message
 
 
     @command(name="set-hitlist-channel", aliases=["shc"], description="Set Hitlist Channel.\nRequired `administrator` permissions.")
     @has_permissions(administrator=True)
-    async def set_hitlist_command(self, ctx: Context, channels : TextChannel) -> None:
+    async def set_hitlist_command(self, ctx: Context, channel : TextChannel) -> None:
         await del_user_msg(ctx)
-        
-        self.bot.GUILD_DB.update_one({"_id": ctx.guild.id}, {"$set": {"Hitlist": channels.id}})
-        await ctx.send(f'Hitlist channel set/update to {channels.mention}', delete_after = 5)
+
+        avatar = await ctx.guild.me.avatar.read()
+        webhook = await channel.create_webhook(name="Hitlist", reason=f"This channel have been set to hitlist channel by {ctx.author}", avatar=avatar)
+        self.bot.GUILD_DB.update_one({"_id": ctx.guild.id}, {"$set": {"Hitlist": {"ID": webhook.id, "Token": webhook.token}}})
+
+        await ctx.send(f'Hitlist channel set/update to {channel.mention}', delete_after = 5)
 
 
     @command(name="set-bounty-channel", aliases=["sbc"], description="Set Bounty Submissions Channel.\nRequired `administrator` permissions.")
     @has_permissions(administrator=True)
-    async def set_bounty_command(self, ctx: Context, channels : TextChannel) -> None:
+    async def set_bounty_command(self, ctx: Context, channel : TextChannel) -> None:
         await del_user_msg(ctx)
-        
-        self.bot.GUILD_DB.update_one({"_id": ctx.guild.id}, {"$set": {"Bounty submission": channels.id}})
-        await ctx.send(f'Bounty Submissions channel set/update to {channels.mention}', delete_after = 5)
+
+        avatar = await ctx.guild.me.avatar.read()
+        webhook = await channel.create_webhook(name="Hitlist", reason=f"This channel have been set to bounty submission channel by {ctx.author}", avatar=avatar)
+        self.bot.GUILD_DB.update_one({"_id": ctx.guild.id}, {"$set": {"Bounty submission": {"ID": webhook.id, "Token": webhook.token}}})
+
+        await ctx.send(f'Bounty Submissions channel set/update to {channel.mention}', delete_after = 5)
 
 
-    @staticmethod  
-    async def del_bounty(msg: Message) -> None:
-        await msg.edit(content="Expired")
+    async def deleteBounty(self, guild: Guild, msgID: int) -> None:     
+        HITLIST = self.bot.GUILD_DB.find_one({"_id": guild.id})
+        webhook = HITLIST["Hitlist"]
+        ID, Token = webhook["ID"], webhook["Token"]
+
+        channel = Webhook.from_url(url=f"https://discord.com/api/webhooks/{ID}/{Token}", session=self.bot.session)
+        msg = await channel.fetch_message(msgID)
+        await msg.delete()
+
+        self.bot.SCHEDULER.delete_one({"_id": f"{msgID}-Bounty", "Guild ID": guild.id})
 
 
     @command(name="submit-bounty", aliases=['sb'], description="Submit bounty to bounty submission channel.\nRequire `Bounty Hunter` role.")
     @cooldown(rate=4, per=7200, type=BucketType.user)
     @has_role("Bounty Hunter")
     async def _bounty(self, ctx: Context, target: str,  *, reason: str) -> None:
-        CHECK_CHANNEL = await self.check_channel(ctx)
-        await del_user_msg(ctx)
+        CHECK_CHANNEL = await self.checkChannel(ctx)
+        await del_user_msg(ctx) 
 
         if CHECK_CHANNEL:
-            user_name = await self.roblox.get_user_by_username(target)
+            user_name = await self.bot.roblox.get_user_by_username(target)
             if user_name == None:
                 await ctx.send("No user found with that username.")
             else:
-                user = await self.roblox.get_user(user_name.id)
-                thumbnail = await self.roblox.thumbnails.get_user_avatars([user.id], size="720x720")
+                user = await self.bot.roblox.get_user(user_name.id)
+                thumbnail = await self.bot.roblox.thumbnails.get_user_avatars([user.id], size="720x720")
                 thumbnail_url = thumbnail[0].image_url if thumbnail[0].image_url is not None else Embed.Empty
-                expired_time = datetime.now() + timedelta(days=7)
+                expireTime = datetime.now() + timedelta(seconds=60)
 
                 embed = Embed(title=f"{user.name} profiles", color=0x2f3136, url=f"https://www.roblox.com/users/{user.id}/profile")
                 embed.set_image(url=thumbnail_url)
@@ -114,7 +125,7 @@ class Bounty(Cog):
                 fields = [("User Name: ", user.name, True),
                             ("Display Name: ", user.display_name, True),
                             ("Request by: ", ctx.author, False),
-                            ("Expired: ", f"<t:{str(expired_time.timestamp()).split('.')[0]}:R>", True),
+                            ("Expired: ", f"<t:{str(expireTime.timestamp()).split('.')[0]}:R>", True),
                             ("Reason: ", reason, False),]
 
                 for name, value, inline in fields:
@@ -122,16 +133,11 @@ class Bounty(Cog):
 
                 answer, host = await ButtonConfirm(embed).prompt(ctx)
                 if answer is True:
-                    hitlist = await self.get_hitlist(ctx)
-                    if hitlist is None:
-                        await ctx.send("Hitlist channel haven't been specified.", delete_after=5)
-                    else:
-                        embed.set_author(name=f"Host by {host}", icon_url=host.display_avatar)
-                        msg: Message = await hitlist.send(content="<@&878261620311220225>", embed=embed)
+                    msg = await self.sendHitlist(ctx, embed, host)
 
-                        self.bot.scheduler.add_job(self.del_bounty, id=f"{msg.id}-Bounty", args=[msg], next_run_time=expired_time)
-                        self.bot.SCHEDULER.insert_one({"_id": f"{msg.id}-Bounty", "Guild ID": ctx.guild.id,
-                                                   "Expired": (datetime.now() + timedelta(days=7)).strftime('%d-%m-%Y-%H-%M-%S'),})
+                    self.bot.scheduler.add_job(self.deleteBounty, id=f"{msg.id}-Bounty", args=[ctx.guild, msg.id], next_run_time=expireTime)
+                    self.bot.SCHEDULER.insert_one({"_id": f"{msg.id}-Bounty", "Guild ID": ctx.guild.id, "Guild": ctx.guild.name,
+                                                    "Expired": expireTime.strftime('%d-%m-%Y-%H-%M-%S'),})
                 else:
                     pass
         else:
